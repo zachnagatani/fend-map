0ba4e3978b5311b9e66638b8c6fc5db494e9cebf
znagatani <znagatani@gmail.com>
2016-05-03 12:45:23 -0700

    Create to do list with helpful hints. Brainstorm through the project requirements and add reminders of other projects that already have some of this functionality

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+/* TODO:
+1. Add full-screen map to the page. Check out resume tool and
+cross worship center.
+2. Add markers. Check out resume project (I added markers there before)
+3. Create a list with those markers' locations. Check out cat-clicker
+4. Filter option.. figure that out
+5. Use others API's to provide info for the markers. Check ajax moving helper project (wikipedia/nytimes info)
+6. Animations. Find animation libraries to handle this?
+7. Error handling. Check out the ajax moving helper project again for .fail() with jquery ajax/getJSON
+*/
