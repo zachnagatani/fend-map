0f73aca35f9ebdee7557d061b2ca4e1e0dd9e985
znagatani <znagatani@gmail.com>
2016-05-12 20:52:05 -0700

    Chore: Gh-pages commit

--- js/model.js
+++ js/model.js
@@ -1,1 +1,289 @@
-var locationData=[{name:"Pizza My Heart",foursquareVenueID:"4a82147ef964a52082f81fe3",type:"Pizza",coordinates:{lat:37.303856,lng:-121.89722}},{name:"Powell's Sweet Shoppe",foursquareVenueID:"4b11fc05f964a520d78723e3",type:"Candy",coordinates:{lat:37.307686,lng:-121.900156}},{name:"Aqui Cal-Mex",foursquareVenueID:"42cc7080f964a520ea251fe3",type:"Mexican",coordinates:{lat:37.307547,lng:-121.900875}},{name:"Main Street Burgers",foursquareVenueID:"54373d95498e5b5416738404",type:"Burgers",coordinates:{lat:37.308185,lng:-121.900533}},{name:"Bertucelli's La Villa",foursquareVenueID:"4adf64f1f964a520387a21e3",type:"Italian",coordinates:{lat:37.304607,lng:-121.898566}},{name:"Bill's Cafe",foursquareVenueID:"528101f411d2b3a7b8645be8",type:"Breakfast",coordinates:{lat:37.308434,lng:-121.901751}},{name:"Bibo's NY Pizza",foursquareVenueID:"528101f411d2b3a7b8645be8",type:"Pizza",coordinates:{lat:37.306478,lng:-121.891727}},{name:"Blue Water Seafood and Crab",foursquareVenueID:"50ac1ab48acafb3b1f9264eb",type:"Seafood",coordinates:{lat:37.31016,lng:-121.896185}},{name:"Cafe Gourmet",foursquareVenueID:"52f818f0498ed4d1e7d6eb36",type:"Cafe",coordinates:{lat:37.310152,lng:-121.895612}},{name:"Cafe San Jose",foursquareVenueID:"528101f411d2b3a7b8645be8",type:"Breakfast",coordinates:{lat:37.294926,lng:-121.913635}},{name:"Crepevine",foursquareVenueID:"502bc80fe4b0bea491f917d4",type:"Crepes",coordinates:{lat:37.308185,lng:-121.900533}},{name:"Dry Creek Grill",foursquareVenueID:"4f1b9a33e4b0d9f8b9925075",type:"American",coordinates:{lat:37.293331,lng:-121.912942}}];
+// Array of location data
+var locationData = [
+
+	{
+		name: "Pizza My Heart",
+
+		foursquareVenueID: "4a82147ef964a52082f81fe3",
+
+		type: "Pizza",
+
+		coordinates: {
+
+			lat: 37.303856,
+
+			lng: -121.89722,
+		}
+	},
+
+	// {
+	// 	name: "Powell's Sweet Shoppe",
+
+	// 	foursquareVenueID: "4b11fc05f964a520d78723e3",
+
+	// 	type: "Candy",
+
+	// 	coordinates: {
+
+	// 		lat: 37.307686,
+
+	// 		lng: -121.900156,
+	// 	}
+	// },
+
+	{
+		name: "Aqui Cal-Mex",
+
+		foursquareVenueID: "42cc7080f964a520ea251fe3",
+
+		type: "Mexican",
+
+		coordinates: {
+
+			lat: 37.307547,
+
+			lng: -121.900875,
+		}
+	},
+
+	{
+		name: "Main Street Burgers",
+
+		foursquareVenueID: "54373d95498e5b5416738404",
+
+		type: "Burgers",
+
+		coordinates: {
+
+			lat: 37.308185,
+
+			lng: -121.900533,
+		}
+	},
+
+	{
+		name: "Bertucelli's La Villa",
+
+		foursquareVenueID: "4adf64f1f964a520387a21e3",
+
+		type: "Italian",
+
+		coordinates: {
+
+			lat: 37.304607,
+
+			lng: -121.898566,
+		}
+	},
+
+	{
+		name: "Bill's Cafe",
+
+		foursquareVenueID: "528101f411d2b3a7b8645be8",
+
+		type: "Breakfast",
+
+		coordinates: {
+
+			lat: 37.308434,
+
+			lng: -121.901751,
+		}
+	},
+
+	{
+		name: "Bibo's NY Pizza",
+
+		foursquareVenueID: "528101f411d2b3a7b8645be8",
+
+		type: "Pizza",
+
+		coordinates: {
+
+			lat: 37.306478,
+
+			lng: -121.891727,
+		}
+	},
+
+	{
+		name: "Blue Water Seafood and Crab",
+
+		foursquareVenueID: "50ac1ab48acafb3b1f9264eb",
+
+		type: "Seafood",
+
+		coordinates: {
+
+			lat: 37.310160,
+
+			lng: -121.896185,
+		}
+	},
+
+	{
+		name: "Cafe Gourmet",
+
+		foursquareVenueID: "52f818f0498ed4d1e7d6eb36",
+
+		type: "Cafe",
+
+		coordinates: {
+
+			lat: 37.310152,
+
+			lng: -121.895612,
+		}
+	},
+
+	{
+		name: "Cafe San Jose",
+
+		foursquareVenueID: "528101f411d2b3a7b8645be8",
+
+		type: "Breakfast",
+
+		coordinates: {
+
+			lat: 37.294926,
+
+			lng: -121.913635,
+		}
+	},
+
+	{
+		name: "Crepevine",
+
+		foursquareVenueID: "502bc80fe4b0bea491f917d4",
+
+		type: "Crepes",
+
+		coordinates: {
+
+			lat: 37.308185,
+
+			lng: -121.900533,
+		}
+	},
+
+	{
+		name: "Dry Creek Grill",
+
+		foursquareVenueID: "4f1b9a33e4b0d9f8b9925075",
+
+		type: "American",
+
+		coordinates: {
+
+			lat: 37.293331,
+
+			lng: -121.912942,
+		}
+	},
+
+	{
+		name: "Edna Ray Chinese Restaurant",
+
+		foursquareVenueID: "4ae75b52f964a520a8aa21e3",
+
+		type: "Chinese",
+
+		coordinates: {
+
+			lat: 37.306937,
+
+			lng: -121.900352,
+		}
+	},
+
+	{
+		name: "Flower Flour",
+
+		foursquareVenueID: "4b9435d1f964a520cc6e34e3",
+
+		type: "Bakery",
+
+		coordinates: {
+
+			lat: 37.310140,
+
+			lng: -121.896966,
+		}
+	},
+
+	{
+		name: "Great Wall Restaurant",
+
+		foursquareVenueID: "4b36baeef964a5207b3b25e3",
+
+		type: "Chinese",
+
+		coordinates: {
+
+			lat: 37.306566,
+
+			lng: -121.890645,
+		}
+	},
+
+	{
+		name: "Jake's of Willow Glen",
+
+		foursquareVenueID: "42cc7080f964a520ec251fe3",
+
+		type: "Pizza",
+
+		coordinates: {
+
+			lat: 37.309892,
+
+			lng: -121.901521,
+		}
+	},
+
+	{
+		name: "John's of Willow Glen",
+
+		foursquareVenueID: "4aa52f0cf964a520884720e3",
+
+		type: "American",
+
+		coordinates: {
+
+			lat: 37.306215,
+
+			lng: -121.899164,
+		}
+	},
+
+	{
+		name: "Le Boulanger",
+
+		foursquareVenueID: "4ad8c2b9f964a5206e1421e3",
+
+		type: "Sandwiches",
+
+		coordinates: {
+
+			lat: 37.304172,
+
+			lng: -121.898244,
+		}
+	},
+
+	{
+		name: "New Indian Cuisine",
+
+		foursquareVenueID: "4b1c765df964a5209c0724e3",
+
+		type: "Indian",
+
+		coordinates: {
+
+			lat: 37.310486,
+
+			lng: -121.897076,
+		}
+	}
+
+];
