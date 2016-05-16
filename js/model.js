60a5b2c63f50d7dcba0d1a11c9ca300c54c3b654
znagatani <znagatani@gmail.com>
2016-05-16 16:22:07 -0700

    chore: GH Commit

--- js/model.js
+++ js/model.js
@@ -1,3 +1,8 @@
+// TODO: Access the Foursquare search or explore API, receive the response of venues, and store them in the model
+// Use that data instead of the current model being used to populate the map with markers
+
+var foursquareVenues = [];
+
 // Array of location data
 var locationData = [
 
