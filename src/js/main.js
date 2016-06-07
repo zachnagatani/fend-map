053c29afbd434edff65fe13e0df7d1eb2881632b
znagatani <znagatani@gmail.com>
2016-06-07 11:18:41 -0700

    Chore: Modularize js files

--- src/js/main.js
+++ src/js/main.js
@@ -1,106 +1,20 @@
-function preLoad() {
-	$(window).load(function() {
-		$(".se-pre-con").fadeOut("slow");
-	});
-
-}
-preLoad();
-
-function googleError() {
-	alert('Google maps failed to load. Please refresh the page and try again.');
-}
-
-function initApplication() {
-	var map;
-	var geocoder;
-	var userCity = "";
-	var userCityGeocode = {};
-	var allMarkers = [];
-	var infoWindow = new google.maps.InfoWindow({
-		content: infoWindowNode
-	});
-	var foursquareVenueNames = [];
-
-	function venueNames() {
-		foursquareVenues.forEach(function(venue){
-			foursquareVenueNames.push(venue.name);
-		});
-
-		console.log(foursquareVenueNames);
-	};
-
-	function styleInfoWindow() {
-		google.maps.event.addListener(infoWindow, 'domready', function() {
-			var iwOuter = $('.gm-style-iw');
-
-			/* The DIV we want to change is above the .gm-style-iw DIV.
-			 * So, we use jQuery and create a iwBackground variable,
-			 * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
-			 */
-			var iwBackground = iwOuter.prev();
-
-			iwBackground.children(':nth-child(2)').css({
-				'display': 'none'
-			});
-
-			iwBackground.children(':nth-child(4)').css({
-				'display': 'none'
-			});
-
-			iwBackground.children(':nth-child(1)').css({
-				'position': 'relative',
-				'z-index': '0'
-			});
-
-			iwBackground.children(':nth-child(3)').find('div').children().css({
-				'box-shadow': 'none',
-				'z-index': '-1',
-				'background': 'rgba(255, 255, 255, 1)',
-				'position': 'relative',
-				'z-index': '0'
-			});
-
-			iwBackground.children(':nth-child(3)').find('div').children().css({
-				'z-index': '1'
-			});
-
-			var iwCloseBtn = iwOuter.next();
-
-			var iwCloseBtnX = iwCloseBtn.next();
-
-			iwCloseBtn.css({
-				width: "25px",
-				height: "25px",
-				opacity: '1', // by default the close button has an opacity of 0.7
-				right: '25px',
-				top: '6px', // button repositioning
-				border: '1px solid #019CDE', // increasing button border and new color
-				'border-radius': '13px', // circular effect
-				'box-shadow': '0 0 5px #f0f0f0', // 3D effect to highlight the button
-				background: '#fff'
-			});
-
-			iwCloseBtn.children(':nth-child(1)').css({
-				top: '-330px',
-				left: '3px'
-			});
-
-			iwCloseBtn.mouseout(function() {
-				$(this).css({
-					opacity: '1'
-				});
-			});
-		});
-	}
-
-	styleInfoWindow();
-
+function initViewModel(){
 	var ViewModel = {
 		foursquareVenues: ko.observableArray([]),
 		foursquareVenuesList: ko.observableArray([foursquareVenues]),
 		markerList: ko.observableArray([allMarkers]),
 		query: ko.observable(''),
 		foursquareVenuesFilter: ko.observable(),
+
+		foursquareVenueNames: [],
+
+	venueNames: function() {
+		foursquareVenues.forEach(function(venue){
+			ViewModel.foursquareVenueNames.push(venue.name);
+		});
+
+		console.log(ViewModel.foursquareVenueNames);
+	},
 
 		stopRefreshOnEnter: $(function() {
 		  	  $("form").submit(function() { return false; });
@@ -177,7 +91,7 @@ function initApplication() {
 			ViewModel.venueName(ViewModel.foursquareVenues()[index].name);
 
 			// Grab the name from the venue to grab the correct index
-			var indexByName = foursquareVenueNames.indexOf(ViewModel.venueName());
+			var indexByName = ViewModel.foursquareVenueNames.indexOf(ViewModel.venueName());
 
 			// Use correct index, otherwise index will be wrong when filter is in place
 			infoWindow.open(map, allMarkers[indexByName]);
@@ -338,14 +252,10 @@ function initApplication() {
 				}
 
 				function toggleBounce() {
-					if (marker.getAnimation() !== null) {
+					marker.setAnimation(google.maps.Animation.BOUNCE);
+					setTimeout(function() {
 						marker.setAnimation(null);
-					} else {
-						marker.setAnimation(google.maps.Animation.BOUNCE);
-						setTimeout(function() {
-							marker.setAnimation(null);
-						}, 750);
-					}
+					}, 750);
 				}
 
 				marker.addListener('click', function() {
@@ -389,7 +299,7 @@ function initApplication() {
 							console.log(ViewModel.foursquareVenues());
 						})();
 						ViewModel.userAddress(userCity);
-						venueNames();
+						ViewModel.venueNames();
 					}
 
 				}).fail(function() {
@@ -403,5 +313,4 @@ function initApplication() {
 	ko.applyBindings(ViewModel);
 
 	ViewModel.subscribeToSearch();
-
-} //initApplication Closing Brace
+} //initVieModel closing brace
