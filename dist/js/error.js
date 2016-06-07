3b476c34b22ca8e45a25fa8505deff248816ff1e
znagatani <znagatani@gmail.com>
2016-06-07 16:10:55 -0700

    add dist

---
+++ dist/js/error.js
@@ -0,0 +1,1 @@
+function googleError(){alert("Google maps failed to load. Please refresh the page and try again.")}
---
+++ dist/js/preload.js
@@ -0,0 +1,1 @@
+function preLoad(){$(window).load(function(){$(".se-pre-con").fadeOut("slow")})}preLoad();
