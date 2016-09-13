3c12ad24b1d4a46300a19cf0fd191cbb71a8f2db
znagatani <znagatani@gmail.com>
2016-09-13 15:57:23 -0700

    Feat: Google Places Autocomplete

--- src/js/init.js
+++ src/js/init.js
@@ -4,6 +4,13 @@ function initApplication() {
 	userCity = '';
 	userCityGeocode = {};
 	allMarkers = [];
-	
+
+	function initAutoComplete() {
+		var autocomplete = new google.maps.places.Autocomplete(
+			(document.getElementById('intro-search-input')));
+	}
+
+	initAutoComplete();
+
 	initViewModel();
 } //initApplication Closing Brace
