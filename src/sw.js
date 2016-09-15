77e07ad77199320c0cf878c50c4bb5ced33dd339
znagatani <znagatani@gmail.com>
2016-09-15 10:56:43 -0700

    Feat: Multiple Caches Caches for venue list and venue info

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,19 @@
 var staticCache = 'feastOn-static-cache-v1';
-var apiCache = 'feastOn-api-cache-v1';
+var venueCache = 'feastOn-venue-cache-v1';
+var venueInfoCache = 'feastOn-venue-info-cache-v1';
+
+function fetchAndCache(myCache, request) {
+	return caches.open(myCache).then(function(cache) {
+		return cache.match(request.url).then(function(response) {
+			if (response) return response;
+			
+			return fetch(request).then(function(networkResponse) {
+				cache.put(request.url, networkResponse.clone());
+				return networkResponse;
+			});
+		});
+	});
+}
 
 this.addEventListener('install', function(event) {
 	event.waitUntil(
@@ -14,63 +28,20 @@ this.addEventListener('install', function(event) {
 	);
 });
 
-// this.addEventListener('fetch', function(event) {
-// 	event.respondWith(apiCache(event.request));
-// });
-
-// function staticCacheCheck(request) {
-// 	return caches.open(staticCacheCheck)
-// }
-
-// function apiCache(request) {
-// 	return caches.open(apiCache).then(function(cache) {
-// 		return cache.match(request.url).then(function(reponse) {
-// 			if (response) return response;
-
-// 			return fetch(request).then(function(networkResponse) {
-// 				cache.put(request.url, networkResponse.clone());
-// 				return networkResponse;
-// 			})
-// 		})
-// 	})
-// }
-
 this.addEventListener('fetch', function(event) {
 	var requestUrl = new URL(event.request.url);
 
-	if (requestUrl.href.startsWith('http://localhost:9999/')) {
-		event.respondWith(grabStatic(event.request));
-	}
-
-	if (requestUrl.href.startsWith('https://api.foursquare.com/') || 
+	if (requestUrl.href.startsWith('http://localhost:9999/')  || 
 		requestUrl.href.startsWith('http://fonts.googleapis.com/') || 
 		requestUrl.href.startsWith('https://cdnjs.cloudflare.com/')) {
-		event.respondWith(grabStatic(event.request));
+		event.respondWith(fetchAndCache(staticCache, event.request));
+	}
+
+	if (requestUrl.href.startsWith('https://api.foursquare.com/') && requestUrl.href.startsWith('https://api.foursquare.com/v2/venues/explore')) {
+		event.respondWith(fetchAndCache(venueCache, event.request));
+	}
+
+	if (requestUrl.href.startsWith('https://api.foursquare.com/') && !requestUrl.href.startsWith('https://api.foursquare.com/v2/venues/explore')) {
+		event.respondWith(fetchAndCache(venueInfoCache, event.request));
 	}
 });
-
-function grabScript(request) {
-	return caches.open(apiCache).then(function(cache) {
-		return cache.match(request.url).then(function(response) {
-			if (response) return response;
-			
-			return fetch(request).then(function(networkResponse) {
-				cache.put(request.url, networkResponse.clone());
-				return networkResponse;
-			});
-		});
-	});
-}
-
-function grabStatic(request) {
-	return caches.open(staticCache).then(function(cache) {
-		return cache.match(request.url).then(function(response) {
-			if (response) return response;
-			
-			return fetch(request).then(function(networkResponse) {
-				cache.put(request.url, networkResponse.clone());
-				return networkResponse;
-			});
-		});
-	});
-}
